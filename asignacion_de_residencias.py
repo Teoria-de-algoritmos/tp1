cce3b6a039ea191012c3dcf55aa5df592c94194f
Alejo Cova <covaalejo@gmail.com>
2017-04-14 20:24:49 -0300

    Datos ingresados por linea de comandos
    Se agregan los datos por la linea de comandos y se imprime informacion
    sobre cada dato de salida

--- asignacion_de_residencias.py
+++ asignacion_de_residencias.py
@@ -5,12 +5,18 @@ from pprint import pprint ## Agarra una matriz y la imprime linda
 def shuffled_list(l):
     shuffle(l)
     return l
-students_quant, hospitals_quant = 10, 2 ## cantidades
-vacancies_quant = 3 ## Maxima cantidad de vacantes
+
+students_quant = input("Ingresar la cantidad de estudiantes: ")
+hospitals_quant = input("Ingresar la cantidad de hospitales: ")
+vacancies_quant = input("Ingresar la cantidad de vacantes maxima: ")
+
 ## Con students_quant = hospitals_quant = 100000 esto llena la ram y traba la maquina, ni se gasten
 ## El limite es students_quant * hospitals_quant <= 10**8
 E = [shuffled_list(range(hospitals_quant)) for _ in xrange(students_quant)] ## Preferencias de cada estudiante
 H = [shuffled_list(range(students_quant)) for _ in xrange(hospitals_quant)] ## Orden de merito
 Q = [choice(range(1,vacancies_quant+1)) for _ in xrange(hospitals_quant)] ## Cantidad de vacantes por hospital
+                                                                          ## 
+print("Preferencia de estudiantes:")                                                                          
 pprint(E)
+print("Orden de merito:")
 pprint(H)
