6fc62bca15669df3958c70e463ce5bf9c7a9ae57
Nicolas <nkeklikian@gmail.com>
2017-04-16 18:34:37 -0300

    El archivo de salida se imprime con el formato del enunciado. Agrego una variacion de Gale-Shapley que resuelve el problema. Agrego la opcion de ejecutarse con datos hardcodeados.

--- asignacion_de_residencias.py
+++ asignacion_de_residencias.py
@@ -1,14 +1,18 @@
-from random import shuffle, choice ## Shuffle desordena una lista, no devuelve nada
-## Choice devuelve un elemento random de una lista
+from random import shuffle, choice ## Shuffle desordena una lista, no devuelve nada. Choice devuelve un elemento random de una lista
 from pprint import pprint ## Agarra una matriz y la imprime linda
-## Agarra una lista y la devuelve desordenada
-def shuffled_list(l):
+
+def shuffled_list(l): ## Agarra una lista y la devuelve desordenada
     shuffle(l)
     return l
-
-students_quant = input("Ingresar la cantidad de estudiantes: ")
-hospitals_quant = input("Ingresar la cantidad de hospitales: ")
-vacancies_quant = input("Ingresar la cantidad de vacantes maxima: ")
+pedir_input = False
+if pedir_input:
+	students_quant = input("Ingresar la cantidad de estudiantes: ")
+	hospitals_quant = input("Ingresar la cantidad de hospitales: ")
+	vacancies_quant = input("Ingresar la cantidad de vacantes maxima: ")
+else :
+	students_quant = 5
+	hospitals_quant = 5
+	vacancies_quant = 3
 
 ## Con students_quant = hospitals_quant = 100000 esto llena la ram y traba la maquina, ni se gasten
 ## El limite es students_quant * hospitals_quant <= 10**8
@@ -20,21 +24,43 @@ print("Preferencia de estudiantes:")
 pprint(E)
 print("Orden de merito:")
 pprint(H)
+print("Cantidad de vacantes por hospital:")
+print Q
  
+def gale_shapley():
+	from collections import defaultdict
+	hospitals = defaultdict(list)
+	engaged = [False] * students_quant
+	students = [0] * students_quant
+	while False in engaged:
+		print engaged
+		print hospitals
+		for stud in xrange(students_quant):
+			if not engaged[stud]:
+				hospitals[E[stud][students[stud]]].append(stud)
+		for hosp in xrange(hospitals_quant):
+			hosp_props = hospitals[hosp]
+			hosp_props.sort(key = lambda x : H[hosp].index(x))
+			for i in xrange(min(Q[hosp],len(hosp_props))):
+				engaged[hosp_props[i]] = True
+			for i in xrange(Q[hosp], len(hosp_props)):
+				students[hosp_props[i]] += 1
+			hosp_props = hosp_props[:min(len(hosp_props),Q[hosp])]
+	return [(i, E[i][students[i]]) for i in xrange(students_quant)]
+
+print gale_shapley()
+
+
 with open("output_asignacion_de_residencias.txt","w") as file: 
 
 	file.write(str(students_quant) + "\n")
 
-	for i in range(students_quant):
-		file.write(str(E[i]) + " ")
-	file.write("\n")
+	for student in E:
+		file.write("".join(str(pref) + " " for pref in student) + "\n")
 
-	file.write(str(hospitals_quant)+"\n")
+	file.write(str(hospitals_quant) + "\n")
 
-	for i in range(hospitals_quant):
-		file.write(str(H[i]) + " ")
-	file.write("\n")
+	for hosp in H:
+		file.write("".join(str(merit) + " " for merit in hosp) + "\n")
 
-	for i in range(hospitals_quant):
-		file.write(str(Q[i]) + " ")
-	file.write("\n")
+	file.write("".join(str(vac) + " " for vac in Q) + "\n")
