4f6fe8410bb873274fc922d4e49a5917876bdffe
NKeklikian <nkeklikian@gmail.com>
2017-04-21 02:52:44 -0300

    Renombro variables, evito que se vuelvan a calcular los matrimonios cada vez que se consulta

--- asignacion_de_residencias.py
+++ asignacion_de_residencias.py
@@ -1,5 +1,6 @@
 from random import shuffle, choice ## Shuffle desordena una lista, no devuelve nada. Choice devuelve un elemento random de una lista
 #from pprint import pprint ## Agarra una matriz y la imprime linda
+from collections import defaultdict
 import timeit
 class Residencias:
     
@@ -7,23 +8,24 @@ class Residencias:
         shuffle(l)
         return l
     
-    def __init__(self, pedir_input):
+    def __init__(self, pedir_input=False):
         self.time = timeit.default_timer()
+        self.resultado = []
         #print self.time
         if pedir_input:
-            self.students_quant = input("Ingresar la cantidad de estudiantes: ")
-            self.hospitals_quant = input("Ingresar la cantidad de hospitales: ")
-            self.vacancies_quant = input("Ingresar la cantidad de vacantes maxima: ")
+            self.estudiantes = input("Ingresar la cantidad de estudiantes: ")
+            self.hospitales = input("Ingresar la cantidad de hospitales: ")
+            self.vacantes = input("Ingresar la cantidad de vacantes maxima: ")
         else :
-            self.students_quant = 500
-            self.hospitals_quant = 500
-            self.vacancies_quant = 3
+            self.estudiantes = 100
+            self.hospitales = 100
+            self.vacantes = 1
         
         ## Con students_quant = hospitals_quant = 100000 esto llena la ram y traba la maquina, ni se gasten
         ## El limite es students_quant * hospitals_quant <= 10**8
-        self.E = [self.mezclar_lista(range(self.hospitals_quant)) for _ in xrange(self.students_quant)] ## Preferencias de cada estudiante
-        self.H = [self.mezclar_lista(range(self.students_quant)) for _ in xrange(self.hospitals_quant)] ## Orden de merito
-        self.Q = [choice(range(1,self.vacancies_quant+1)) for _ in xrange(self.hospitals_quant)] ## Cantidad de vacantes por hospital    
+        self.E = [self.mezclar_lista(range(self.hospitales)) for _ in xrange(self.estudiantes)] ## Preferencias de cada estudiante
+        self.H = [self.mezclar_lista(range(self.estudiantes)) for _ in xrange(self.hospitales)] ## Orden de merito
+        self.Q = [choice(range(1,self.vacantes+1)) for _ in xrange(self.hospitales)] ## Cantidad de vacantes por hospital    
         #print("Preferencia de estudiantes:")                                                                          
         #pprint(E)
         #print("Orden de merito:")
@@ -34,18 +36,25 @@ class Residencias:
         self.escribir_archivo("output_asignacion_de_residencias.txt")
         #print timeit.default_timer() - self.time
     def reducir(self):
-        h = self.hospitals_quant
-        for hosp in xrange(self.hospitals_quant):
-            for _ in xrange(1, self.Q[hosp]):
-                for s in xrange(self.students_quant):
-                    self.E[s].insert(self.E[s].index(hosp), h)
-                self.H.append(self.H[hosp])
-                self.equivalencias[h] = hosp
-                h += 1
-        return (self.students_quant, h, self.E, self.H)
+        from copy import deepcopy
+        hombres = defaultdict(list)
+        mujeres = deepcopy(self.H)
+        dic_m = defaultdict(list)
+        m_cant = self.hospitales
+        for m in xrange(self.hospitales):
+            dic_m[m].append(m)
+            for _ in xrange(1, self.Q[m]):
+                dic_m[m].append(m_cant)
+                mujeres.append(mujeres[m])
+                self.equivalencias[m_cant] = m
+                m_cant += 1
+        for h in xrange(self.estudiantes):
+            for m in xrange(self.hospitales):
+                for i_m in mujeres[self.E[h][m]]:
+                    hombres[h].append(i_m)
+        return (self.estudiantes, m_cant, [hombres[h] for h in xrange(self.estudiantes)], mujeres)
      
     def gale_shapley(self, hombres, mujeres, pref_hombre, pref_mujer):
-        from collections import defaultdict
         prop_mujer = defaultdict(list)
         pareja_hombre = [-1] * hombres
         sig_prop_hombre = [0] * hombres
@@ -66,20 +75,21 @@ class Residencias:
         return [pref_hombre[i][sig_prop_hombre[i]] for i in xrange(hombres)]
     
     def obtener_resultados(self):
-        resultado = self.gale_shapley(*self.reducir())
-        for i in xrange(len(resultado)):
-            if resultado[i] in self.equivalencias:
-                resultado[i] = self.equivalencias[resultado[i]]
-        return resultado
+        if not self.resultado:
+            self.resultado = self.gale_shapley(*self.reducir())
+            for i in xrange(len(self.resultado)):
+                if self.resultado[i] in self.equivalencias:
+                    self.resultado[i] = self.equivalencias[self.resultado[i]]
+        return self.resultado
     
     def escribir_archivo(self, ruta):
         with open("output_asignacion_de_residencias.txt","w") as archivo: 
-            archivo.write(str(self.students_quant) + "\n")
-            for student in self.E:
-                archivo.write("".join(str(pref) + " " for pref in student) + "\n")
-            archivo.write(str(self.hospitals_quant) + "\n")
-            for hosp in self.H:
-                archivo.write("".join(str(merit) + " " for merit in hosp) + "\n")
+            archivo.write(str(self.estudiantes) + "\n")
+            for estudiante in self.E:
+                archivo.write("".join(str(pref) + " " for pref in estudiante) + "\n")
+            archivo.write(str(self.hospitales) + "\n")
+            for hospital in self.H:
+                archivo.write("".join(str(merito) + " " for merito in hospital) + "\n")
             archivo.write("".join(str(vac) + " " for vac in self.Q) + "\n")
     
 #r1 = Residencias(False)
