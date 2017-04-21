39823bc0c180696c41b61d05d1c23167a69ac441
NKeklikian <nkeklikian@gmail.com>
2017-04-21 03:11:03 -0300

    Convierto la funcion en una clase con metodos de consulta

--- comunidades_en_redes.py
+++ comunidades_en_redes.py
@@ -1,34 +1,48 @@
-from collections import defaultdict, deque ## diccionario con valor por default, cola optimizada
+from collections import deque ## cola optimizada
+from collections import defaultdict
 from GrafoUtils import Digrafo
 
-grafo = Digrafo.parse("Archivos/Problema 3/d3.txt")
+class Kosaraju:
     
-def componentes_fuertemente_conexas(grafo, vertices):
-    ## Devuelve la cantidad de componentes conexas
-    ## Utilizando el algoritmo de Kosaraju
-    visitado = defaultdict(bool)
-    L = deque([])
- 
-    def visitar(u):
-        if not visitado[u]:
-            visitado[u] = True
-            for v in grafo.vecinos_entrantes(u):
-                visitar(v)
-            L.appendleft(u)
- 
-    for u in xrange(vertices):
-        visitar(u)
- 
-    asignado = defaultdict(lambda:-1)
- 
-    def asignar(u, raiz):
-        if asignado[u] == -1:
-            asignado[u] = raiz
-            for v in grafo.vecinos_salientes(u):
-                asignar(v, raiz)
-    for u in L:
-        asignar(u,u)
+    def __init__(self, ruta):
+        self.grafo = Digrafo.parse(ruta)
+        self.componentes = defaultdict(list)
+        self.componentes_fuertemente_conexas(self.grafo.vertices())
+        
+    def componentes_fuertemente_conexas(self, vertices):
+        ## Devuelve la cantidad de componentes conexas
+        ## Utilizando el algoritmo de Kosaraju
+        visitado = [False] * vertices
+        L = deque([])
+     
+        def visitar(u):
+            if not visitado[u]:
+                visitado[u] = True
+                for v in self.grafo.vecinos_entrantes(u):
+                    visitar(v)
+                L.appendleft(u)
+     
+        for u in xrange(vertices):
+            visitar(u)
+     
+        asignado = [-1] * vertices
+     
+        def asignar(u, raiz):
+            if asignado[u] == -1:
+                asignado[u] = raiz
+                for v in self.grafo.vecinos_salientes(u):
+                    asignar(v, raiz)
+        for u in L:
+            asignar(u,u)
+        
+        for i in xrange(vertices):
+            self.componentes[asignado[i]].append(i)
 
-    return len(set(asignado[i] for i in xrange(vertices)))
-
-print componentes_fuertemente_conexas(grafo, grafo.vertices())
+    def cantidad_componentes(self):
+        return len(self.componentes)
+    
+    def componente_dado_elemento(self, elemento):
+        return self.componentes[self.asignado[elemento]]
+    
+#k1 = Kosaraju("Archivos/Problema 3/d2.txt")
+#print k1.cantidad_componentes()
